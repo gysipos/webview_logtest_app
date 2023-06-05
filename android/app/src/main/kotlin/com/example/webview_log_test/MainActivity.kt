cbf9064de35ba8e39c3552a206b8d5c3542cdc17
siposgyula <gysipos@skawa.hu>
2023-06-05 12:27:59 +0200

    there you go Atesz

---
+++ android/app/src/main/kotlin/com/example/webview_log_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.webview_log_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late final WebViewController _webViewController;
+  late final TextEditingController _textEditingController;
+  List<String> history = [];
+  late final SharedPreferences sp;
+
+  @override
+  void initState() {
+    super.initState();
+    SharedPreferences.getInstance().then((value) {
+      sp = value;
+      setState(() {
+        history = sp.getStringList(historyKey) ?? [];
+      });
+    });
+    _textEditingController = TextEditingController();
+    _webViewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(NavigationDelegate(
+        onPageStarted: (url) {
+          print('onPageStarted: $url //wwapp');
+        },
+        onUrlChange: (change) {
+          print('onUrlChange: ${change.url} //wwapp');
+        },
+        onProgress: (progress) {
+          print('onProgress: $progress //wwapp');
+        },
+        onWebResourceError: (error) {
+          print('onWebResourceError: $error //wwapp');
+        },
+        onPageFinished: (url) async {
+          print('onPageFinished: $url //wwapp');
+        },
+        onNavigationRequest: (request) async {
+          print('onNavigationRequest: ${request.url} //wwapp');
+          return NavigationDecision.navigate;
+        },
+      ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        height: MediaQuery.sizeOf(context).height,
+        child: WebViewWidget(
+          controller: _webViewController,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) => Dialog(
+                    child: SizedBox(
+                      height: 600,
+                      child: CustomScrollView(
+                        slivers: [
+                          SliverList(
+                            delegate: SliverChildListDelegate([
+                              TextField(
+                                decoration: InputDecoration(
+                                    hintText: 'url',
+                                    border: OutlineInputBorder()),
+                                controller: _textEditingController,
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  history = [
+                                    _textEditingController.text,
+                                    ...history
+                                  ];
+                                  sp.setStringList(historyKey, history);
+                                  Navigator.pop(context);
+                                  _webViewController.loadRequest(Uri.parse(
+                                      addHttpStuff(
+                                          _textEditingController.text)));
+                                },
+                                child: const Text('tőccsed'),
+                              ),
+                            ]),
+                          ),
+                          SliverList(
+                              delegate: SliverChildBuilderDelegate(
+                                  childCount: history.length, (context, index) {
+                            return TextButton(
+                                onPressed: () {
+                                  _webViewController.loadRequest(
+                                      Uri.parse(addHttpStuff(history[index])));
+                                  Navigator.pop(context);
+                                },
+                                child: Text(history[index]));
+                          })),
+                          SliverList(
+                              delegate: SliverChildListDelegate([
+                            ElevatedButton(
+                                onPressed: () {
+                                  sp.clear();
+                                  setState(() {
+                                    history = [];
+                                  });
+                                },
+                                child: const Text('elfelejtsed!'))
+                          ]))
+                        ],
+                      ),
+                    ),
+                  ));
+        },
+        child: const Icon(Icons.send),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+String addHttpStuff(String url) {
+  if (url.startsWith('http://') || url.startsWith('https://')) {
+    return url;
+  } else {
+    return 'https://$url';
+  }
+}
+
+const historyKey = 'history';
